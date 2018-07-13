55134b4c5b40654c091375f830132b91de5d2704
Александр Зепп <The_Java@mail.ru>
2018-07-14 00:00:39 +0400

    Change magick to heart

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,6 +2,6 @@
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 loglevel=0 buildvariant=userdebug
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_PREBUILT_KERNEL := device/nokia/magick/kernel
+TARGET_PREBUILT_KERNEL := device/nokia/heart/kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 #BOARD_CUSTOM_BOOTIMG := true
