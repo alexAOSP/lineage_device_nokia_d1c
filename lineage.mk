e4f6091b00bfee7c673a0ca387e26f984dad9bb0
Александр Зепп <The_Java@mail.ru>
2018-07-13 23:49:59 +0400

    Change magick to heart

--- lineage.mk
+++ lineage.mk
@@ -1,12 +1,12 @@
-$(call inherit-product, device/nokia/magick/full_magick.mk)
+$(call inherit-product, device/nokia/heart/full_heart.mk)
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := lineage_magick
+PRODUCT_NAME := lineage_heart
 BOARD_VENDOR := nokia
-PRODUCT_DEVICE := magick
+PRODUCT_DEVICE := heart
 
 PRODUCT_GMS_CLIENTID_BASE := android-nokia
 
@@ -15,4 +15,4 @@ PRODUCT_MANUFACTURER := HMD Global
 PRODUCT_BRAND := Nokia
 TARGET_VENDOR := nokia
 TARGET_VENDOR_PRODUCT_NAME := Nokia5
-TARGET_VENDOR_DEVICE_NAME := magick
+TARGET_VENDOR_DEVICE_NAME := heart
