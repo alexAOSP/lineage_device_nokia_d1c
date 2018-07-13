60033ebb578678dc5942c4f04295a23643a743a1
Александр Зепп <The_Java@mail.ru>
2018-07-13 23:50:41 +0400

    Change magick to heart

--- full_heart.mk
+++ full_heart.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from marmite device
-$(call inherit-product, device/nokia/magick/device.mk)
+$(call inherit-product, device/nokia/heart/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := magick
-PRODUCT_NAME := full_magick
+PRODUCT_DEVICE := heart
+PRODUCT_NAME := full_heart
 PRODUCT_BRAND := Nokia
 PRODUCT_MODEL := Nokia 5
 PRODUCT_MANUFACTURER := HMD Global
